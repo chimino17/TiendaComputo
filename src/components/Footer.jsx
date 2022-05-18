3d2afa1dc1b2be15695a9c945b84ddcb283607c5
Manuel <chimino316@hotmail.com>
2022-05-17 22:28:24 -0500

    primer commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/components/Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <p className="Footer-title">Manuel Alejandro</p>
+      <p className="Footer-copy">chimino316@hotmail.com</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/initialState.js
@@ -0,0 +1,61 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  cart: [],
+  buyer: [],
+  orders: [],
+  products: [
+    {
+      id: "1",
+      image:
+        "https://www.cyberpuerta.mx/img/product/M/CP-LENOVO-81MA001EPD-1.png",
+      title: "Laptop Lenovo 100e Chromebook 2da Gen",
+      price: 2749,
+      description:
+        '11.6" HD, Intel Celeron N4020 1.10GHz, 4GB, 32GB, Chrome OS, Español, Negro',
+    },
+    {
+      id: "3",
+      image:
+        "https://www.cyberpuerta.mx/img/product/M/CP-ASUS-90NX0FU1-M01010-1.jpg",
+      title: 'Laptop ASUS VivoBook W202 11.6" HD',
+      price: 4409,
+      description:
+        "Intel Celeron N3350 1.10GHz, 4GB, 64GB, Windows 10 Pro 64-bit, Español, Azul",
+    },
+    {
+      id: "4",
+      image:
+        "https://www.cyberpuerta.mx/img/product/M/CP-ASUS-F543MA-CEl4G500WH-02-1.jpg",
+      title: 'Laptop ASUS F543MA 15.6" Full HD, Intel Celeron',
+      price: 6529,
+      description:
+        "Intel Celeron N4020 1.10GHz, 4GB, 500GB, Windows 10 Home 64-bit, Español, Plata",
+    },
+    {
+      id: "5",
+      image:
+        "https://www.cyberpuerta.mx/img/product/M/CP-GATEWAY-GWTN141-6PR-03ff11.jpg",
+      title: 'Laptop Gateway GWTN141-6 14.1" Full HD',
+      price: 8439,
+      description:
+        "Intel Core i3-1115G4 3GHz, 4GB, 128GB SSD, Windows 10 Home S 64-bit, Inglés, Púrpura",
+    },
+    {
+      id: "6",
+      image:
+        "https://www.cyberpuerta.mx/img/product/M/CP-MSI-GF63THIN10SCXR-222-1.jpg",
+      title: 'Laptop Gamer MSI GF63 Thin 15.6" Full HD',
+      price: 18039,
+      description:
+        "Intel Core i5-10300H 2.50GHz, 8GB, 256GB SSD, NVIDIA GeForce GTX 1650 Max-Q, Windows 10 Home, Inglés",
+    },
+    {
+      id: "7",
+      image:
+        "https://www.cyberpuerta.mx/img/product/M/CP-APPLE-MKGT3EA-f6e61c.jpg",
+      title: "Apple MacBook Pro Retina MKGT3E/A 14",
+      price: 62319,
+      description: "Apple M1 Pro, 16GB, 1TB SSD, Plata (Octubre 2021)",
+    },
+  ],
+};
